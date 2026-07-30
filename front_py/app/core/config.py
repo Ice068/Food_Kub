ae5582c8e4f805882ad83a8f1b4ed5b540d5a1c0
Tanatap Treerapragon <thanathep.ice@gmail.com>
2026-07-30 10:35:55 +0700

    Delete front_py directory

--- front_py/app/core/config.py
+++
@@ -1,9 +0,0 @@
-class Settings:
-  """เก็บค่า config ของระบบ"""
-  APP_TITLE: str = "ระบบสั่งร้านอาหาร"
-  TEMPLATES_DIR: str = "templates"
-  STATIC_DIR: str = "static"
-  STATIC_URL: str = "/static"
-
-
-settings = Settings()
